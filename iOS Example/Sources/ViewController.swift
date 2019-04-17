04479c14530865c1126c30287d2649ee6f7df896
lihui <lihui@chessease.com>
2019-04-17 08:53:43 +0800

    加入Nuke支持

--- iOS Example/Sources/ViewController.swift
+++ iOS Example/Sources/ViewController.swift
@@ -24,7 +24,7 @@ class IOSViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     
---
+++ iOS Example/Sources/iOS Example-Bridging-Header.h
@@ -0,0 +1,6 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+// MARK: - WebP解码
+#import <WebP/WebPDataDecoder.h>
